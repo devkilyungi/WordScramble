b427de46752cc3644a240cfa3ec93caad21fa42c
devkilyungi <victorkilyungi@gmail.com>
2024-05-20 22:28:41 +0300

    feat: Add word length choice option in settings

---
+++ WordScramble/Screens/SettingsScreen.swift
@@ -0,0 +1,89 @@
+//
+//  ScoreScreen.swift
+//  WordScramble
+//
+//  Created by Victor Kilyungi on 20/05/2024.
+//
+
+import SwiftUI
+
+struct SettingsScreen: View {
+    
+    @EnvironmentObject private var viewModel: HomeScreenViewModel
+    
+    @State private var selectedDuration: GameDuration = .oneAndHalfMinutes
+    @State private var showProfile: Bool = false
+    let appVersion = "1.0.0"
+    
+    var body: some View {
+        Form {
+            Section(header: Text("Game Settings")) {
+                VStack(alignment: .leading) {
+                    Text("Word Length: \(viewModel.wordSize.rawValue)")
+                    
+                    Slider(value: Binding(
+                        get: { Double(viewModel.wordSize.rawValue) },
+                        set: {
+                            if let length = WordLength(rawValue: Int($0)) {
+                                viewModel.wordSize = length
+                            } else {
+                                print("Not a valid enum raw value")
+                            }
+                        }
+                    ), in: 6...12, step: 1)
+                }
+                
+                Picker("Timer Length", selection: $selectedDuration) {
+                    ForEach(GameDuration.allCases) { duration in
+                        Text(duration.description).tag(duration)
+                    }
+                }
+                
+                Text("Changes will reflect when you start a new game.")
+                    .foregroundColor(.gray)
+                    .font(.caption)
+            }
+            
+            Section(header: Text("Profile")) {
+                Button(action: {
+                    showProfile.toggle()
+                }) {
+                    HStack {
+                        Image(systemName: "person.circle")
+                        Text("User Profile")
+                    }
+                }
+                .sheet(isPresented: $showProfile) {
+                    UserProfileView()
+                }
+            }
+            
+            Section(header: Text("App Information")) {
+                HStack {
+                    Text("App Version")
+                    Spacer()
+                    Text(appVersion)
+                        .foregroundColor(.gray)
+                }
+            }
+        }
+        .navigationTitle("Settings")
+    }
+}
+
+struct UserProfileView: View {
+    var body: some View {
+        VStack {
+            Text("User Profile")
+                .font(.largeTitle)
+                .padding()
+            
+            // User profile details can be added here
+            Text("Username: ExampleUser")
+            Text("Email: example@example.com")
+            
+            Spacer()
+        }
+        .padding()
+    }
+}
---
+++ WordScramble/Screens/ViewModels/HomeScreenViewModel.swift
@@ -0,0 +1,221 @@
+//
+//  HomeScreenViewModel.swift
+//  WordScramble
+//
+//  Created by Victor Kilyungi on 20/05/2024.
+//
+
+import SwiftUI
+import Combine
+
+@MainActor
+class HomeScreenViewModel: ObservableObject {
+    
+    @Published var usedWords = [String]()
+    @Published var rootWord = "Scramble"
+    @Published var newWord = ""
+    @Published var score = 0
+    @Published var wordSize: WordLength = .eight
+    
+    @Published var gameStarted = false
+    @Published var gameEnded = false
+    
+    @Published var errorMessage = ""
+    @Published var showingError = false
+    
+    @Published var definitionIsLoading = false
+    @Published var showingDefinition = false
+    @Published var wordDefinition = ""
+    @Published var wordToBeDefined = ""
+    
+    @Published var shouldFocus = false
+    
+    private var timer: AnyCancellable?
+    @Published var selectedTimeRemaining: Int = GameDuration.oneAndHalfMinutes.rawValue
+    
+    let dictionaryAPI = DictionaryAPI()
+    
+    // Function to update time remaining
+    func updateTimeRemaining(to newTimeRemaining: Int) {
+        selectedTimeRemaining = newTimeRemaining
+    }
+    
+    func isValidWord(_ word: String, language: String = "en") -> Bool {
+        let checker = UITextChecker()
+        let range = NSRange(location: 0, length: word.utf16.count)
+        let misspelledRange = checker.rangeOfMisspelledWord(in: word, range: range, startingAt: 0, wrap: false, language: language)
+        return misspelledRange.location == NSNotFound
+    }
+    
+    func startGame() {
+        // Attempt to fetch a random word from the database
+        let wordDatabase = WordDatabase()
+        var randomWord: String?
+        while randomWord == nil || !isValidWord(randomWord!) {
+            randomWord = wordDatabase.getRandomWord(from: "\(wordSize.wordForm)_letter_words")
+        }
+        
+        // Assign the fetched random word to the rootWord property
+        guard let validRandomWord = randomWord else {
+            fatalError("Could not fetch a valid random word from the database.")
+        }
+        rootWord = validRandomWord
+        
+        // Reset other game-related properties
+        withAnimation {
+            usedWords = []
+            newWord = ""
+            score = 0
+        }
+        
+        // Start the game timer
+        startTimer()
+    }
+    
+    func quitGame() {
+        gameStarted = false
+        gameEnded = true
+        shouldFocus = false
+        stopTimer()
+    }
+    
+    func restartGame() {
+        withAnimation {
+            usedWords.removeAll()
+        }
+        startGame()
+    }
+    
+    func addNewWord() {
+        let answer = newWord.lowercased().trimmingCharacters(in: .whitespacesAndNewlines)
+        
+        guard answer.count > 1 else {
+            wordError(message: "You can't have a one letter or blank word!")
+            return
+        }
+        
+        guard isOriginal(word: answer) else {
+            wordError(message: "Be more original")
+            return
+        }
+        
+        guard isPossible(word: answer) else {
+            wordError(message: "You can't spell that word from '\(rootWord)'!")
+            return
+        }
+        
+        guard isReal(word: answer) else {
+            wordError(message: "You can't just make them up, you know!")
+            return
+        }
+        
+        withAnimation {
+            withAnimation {
+                showingError = false
+                usedWords.insert(answer, at: 0)
+            }
+        }
+        score += answer.count
+        newWord = ""
+    }
+    
+    func isOriginal(word: String) -> Bool {
+        !usedWords.contains(word)
+    }
+    
+    func isPossible(word: String) -> Bool {
+        var tempWord = rootWord
+        
+        for letter in word {
+            if let pos = tempWord.firstIndex(of: letter) {
+                tempWord.remove(at: pos)
+            } else {
+                return false
+            }
+        }
+        
+        return true
+    }
+    
+    func isReal(word: String) -> Bool {
+        let checker = UITextChecker()
+        let range = NSRange(location: 0, length: word.utf16.count)
+        let misspelledRange = checker.rangeOfMisspelledWord(in: word, range: range, startingAt: 0, wrap: false, language: "en")
+        
+        return misspelledRange.location == NSNotFound
+    }
+    
+    func wordError(message: String) {
+        withAnimation {
+            errorMessage = message
+            showingError = true
+        }
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 3, execute: {
+            withAnimation {
+                self.showingError = false
+            }
+        })
+    }
+    
+    func fetchDefinition(for word: String) async {
+        withAnimation {
+            definitionIsLoading = true
+            showingDefinition = false
+        }
+        
+        do {
+            let definition = try await dictionaryAPI.fetchDefinition(for: word)
+            wordToBeDefined = word
+            wordDefinition = definition
+            withAnimation {
+                definitionIsLoading = false
+                showingDefinition = true
+            }
+        } catch {
+            let nsError = error as NSError
+            let errorMessage: String
+            if nsError.domain == "No definition found" {
+                errorMessage = "No definition found for '\(word)'"
+            } else {
+                errorMessage = nsError.localizedDescription
+            }
+            withAnimation {
+                definitionIsLoading = false
+            }
+            wordError(message: errorMessage)
+        }
+    }
+    
+    func startTimer() {
+        selectedTimeRemaining = GameDuration.oneAndHalfMinutes.rawValue
+        timer = Timer.publish(every: 1, on: .main, in: .common)
+            .autoconnect()
+            .sink { [weak self] _ in
+                guard let self = self else { return }
+                if self.selectedTimeRemaining > 0 {
+                    self.selectedTimeRemaining -= 1
+                } else {
+                    self.timer?.cancel()
+                }
+            }
+    }
+    
+    func stopTimer() {
+        timer?.cancel()
+    }
+    
+    var formattedTimeRemaining: String {
+        if selectedTimeRemaining >= 60 {
+            let minutes = selectedTimeRemaining / 60
+            let seconds = selectedTimeRemaining % 60
+            return String(format: "%02d:%02d", minutes, seconds)
+        } else {
+            return String(format: "00:%02d", selectedTimeRemaining)
+        }
+    }
+    
+    deinit {
+        timer?.cancel()
+    }
+}
